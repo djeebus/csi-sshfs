e04f3edd8f9697d22c9e6648ed7047c1d888070e
Joe Lombrozo <joe@djeebus.net>
2022-07-30 20:47:59 -0400

    make some of the credential parsing cleaner

--- pkg/sshfs/nodeserver.go
+++ pkg/sshfs/nodeserver.go
@@ -1,317 +1,413 @@
 package sshfs
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"sort"
-	"strings"
+    "fmt"
+    "io"
+    "io/ioutil"
+    "os"
+    "os/exec"
+    "sort"
+    "strings"
 
-	"github.com/golang/glog"
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "github.com/golang/glog"
+    v1 "k8s.io/api/core/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/container-storage-interface/spec/lib/go/csi"
-	"golang.org/x/net/context"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-	"k8s.io/kubernetes/pkg/util/mount"
-	"k8s.io/kubernetes/pkg/volume/util"
+    "github.com/container-storage-interface/spec/lib/go/csi"
+    "golang.org/x/net/context"
+    "google.golang.org/grpc/codes"
+    "google.golang.org/grpc/status"
+    "k8s.io/kubernetes/pkg/util/mount"
+    "k8s.io/kubernetes/pkg/volume/util"
 
-	csicommon "github.com/kubernetes-csi/drivers/pkg/csi-common"
+    csicommon "github.com/kubernetes-csi/drivers/pkg/csi-common"
 )
 
 type nodeServer struct {
-	*csicommon.DefaultNodeServer
-	mounts map[string]*mountPoint
+    *csicommon.DefaultNodeServer
+    mounts map[string]*mountPoint
 }
 
 type mountPoint struct {
-	VolumeId     string
-	MountPath    string
-	IdentityFile string
+    VolumeId     string
+    MountPath    string
+    IdentityFile string
 }
 
 func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublishVolumeRequest) (*csi.NodePublishVolumeResponse, error) {
-	targetPath := req.GetTargetPath()
-	notMnt, e := mount.New("").IsLikelyNotMountPoint(targetPath)
-	if e != nil {
-		if os.IsNotExist(e) {
-			if err := os.MkdirAll(targetPath, 0750); err != nil {
-				return nil, status.Error(codes.Internal, err.Error())
-			}
-			notMnt = true
-		} else {
-			return nil, status.Error(codes.Internal, e.Error())
-		}
-	}
+    targetPath := req.GetTargetPath()
+    notMnt, e := mount.New("").IsLikelyNotMountPoint(targetPath)
+    if e != nil {
+        if os.IsNotExist(e) {
+            if err := os.MkdirAll(targetPath, 0750); err != nil {
+                return nil, status.Error(codes.Internal, err.Error())
+            }
+            notMnt = true
+        } else {
+            return nil, status.Error(codes.Internal, e.Error())
+        }
+    }
 
-	if !notMnt {
-		return &csi.NodePublishVolumeResponse{}, nil
-	}
+    if !notMnt {
+        return &csi.NodePublishVolumeResponse{}, nil
+    }
 
-	mountOptions := req.GetVolumeCapability().GetMount().GetMountFlags()
-	if req.GetReadonly() {
-		mountOptions = append(mountOptions, "ro")
-	}
-	if e := validateVolumeContext(req); e != nil {
-		return nil, e
-	}
+    mountOptions := req.GetVolumeCapability().GetMount().GetMountFlags()
+    if req.GetReadonly() {
+        mountOptions = append(mountOptions, "ro")
+    }
+    config, e := parseVolumeContext(req)
+    if e != nil {
+        return nil, e
+    }
 
-	server := req.GetVolumeContext()["server"]
-	port := req.GetVolumeContext()["port"]
-	if len(port) == 0 {
-		port = "22"
-	}
+    e = Mount(config, targetPath)
+    if e != nil {
+        if os.IsPermission(e) {
+            return nil, status.Error(codes.PermissionDenied, e.Error())
+        }
+        if strings.Contains(e.Error(), "invalid argument") {
+            return nil, status.Error(codes.InvalidArgument, e.Error())
+        }
+        return nil, status.Error(codes.Internal, e.Error())
+    }
+    ns.mounts[req.VolumeId] = &mountPoint{IdentityFile: config.privateKeyPath, MountPath: targetPath, VolumeId: req.VolumeId}
+    return &csi.NodePublishVolumeResponse{}, nil
+}
 
-	user := req.GetVolumeContext()["user"]
-	ep := req.GetVolumeContext()["share"]
-	password := req.GetVolumeContext()["password"]
-	privateKey := req.GetVolumeContext()["privateKey"]
-	sshOpts := req.GetVolumeContext()["sshOpts"]
+type config struct {
+    server, port, share string
+    sshOptsMap          map[string]string
 
-	privateKeyPath := ""
-	if privateKey != "" {
-		secret, e := getPublicKeySecret(privateKey)
-		if e != nil {
-			return nil, e
-		}
-		privateKeyPath, e = writePrivateKey(secret)
-		if e != nil {
-			return nil, e
-		}
-	}
+    username, password string
+    privateKeyPath     string
+}
 
-	e = Mount(user, server, port, ep, targetPath, password, privateKeyPath, sshOpts)
-	if e != nil {
-		if os.IsPermission(e) {
-			return nil, status.Error(codes.PermissionDenied, e.Error())
-		}
-		if strings.Contains(e.Error(), "invalid argument") {
-			return nil, status.Error(codes.InvalidArgument, e.Error())
-		}
-		return nil, status.Error(codes.Internal, e.Error())
-	}
-	ns.mounts[req.VolumeId] = &mountPoint{IdentityFile: privateKeyPath, MountPath: targetPath, VolumeId: req.VolumeId}
-	return &csi.NodePublishVolumeResponse{}, nil
+func missingVolumeContextKeyError(key string) error {
+    return status.Errorf(codes.InvalidArgument, "missing volume context value: %s", key)
+}
+
+func missingSecretKeyError(key string) error {
+    return status.Errorf(codes.InvalidArgument, "missing secret key: %s", key)
+}
+
+func parseVolumeContext(req *csi.NodePublishVolumeRequest) (config, error) {
+    var (
+        config config
+        ok     bool
+
+        configMap = req.GetVolumeContext()
+    )
+
+    if config.server, ok = configMap["server"]; !ok {
+        return config, missingVolumeContextKeyError("server")
+    }
+
+    if config.share, ok = configMap["share"]; !ok {
+        return config, missingVolumeContextKeyError("share")
+    }
+
+    if config.port, ok = configMap["port"]; !ok {
+        config.port = "22"
+    }
+
+    if sshOpts, ok := configMap["sshOpts"]; ok {
+        config.sshOptsMap = parseRawOpts(sshOpts)
+    }
+
+    if err := getCredentials(config, configMap); err != nil {
+        return config, err
+    }
+
+    return config, nil
+}
+
+func parseCredentials(credsPath string, model config) error {
+    credsSecret, e := getSecret(credsPath)
+    if e != nil {
+        return e
+    }
+
+    var ok bool
+    if credsSecret.Type == v1.SecretTypeSSHAuth {
+        model.username, ok = credsSecret.StringData[v1.BasicAuthUsernameKey]
+        if !ok {
+            return missingSecretKeyError(v1.BasicAuthUsernameKey)
+        }
+
+        privateKeyData, ok := credsSecret.Data[v1.SSHAuthPrivateKey]
+        if !ok {
+            return missingSecretKeyError(v1.SSHAuthPrivateKey)
+        }
+
+        model.privateKeyPath, e = writePrivateKey(privateKeyData)
+        return e
+    }
+
+    if credsSecret.Type == v1.SecretTypeBasicAuth {
+        if model.username, ok = credsSecret.StringData[v1.BasicAuthUsernameKey]; !ok {
+            return missingSecretKeyError(v1.BasicAuthUsernameKey)
+        }
+        if model.password, ok = credsSecret.StringData[v1.BasicAuthPasswordKey]; !ok {
+            return missingSecretKeyError(v1.BasicAuthPasswordKey)
+        }
+
+        return nil
+    }
+
+    return status.Errorf(codes.InvalidArgument, "invalid secret type: %s", credsSecret.Type)
+}
+
+func getCredentials(model config, config map[string]string) error {
+    credsPath := config["credentials"]
+    if credsPath != "" {
+        return parseCredentials(credsPath, model)
+    }
+
+    return parseLegacyCreds(config, model)
+}
+
+func parseLegacyCreds(config map[string]string, model config) error {
+    privateKey, ok := config["privateKey"]
+    if !ok {
+        return missingVolumeContextKeyError("privateKey")
+    }
+
+    secret, e := getPublicKeySecret(privateKey)
+    if e != nil {
+        return e
+    }
+    privateKeyData, ok := secret.Data[v1.SSHAuthPrivateKey]
+    if !ok {
+        return missingSecretKeyError(v1.SSHAuthPrivateKey)
+    }
+
+    model.privateKeyPath, e = writePrivateKey(privateKeyData)
+    if e != nil {
+        return e
+    }
+
+    if model.username, ok = config["user"]; !ok {
+        return missingVolumeContextKeyError("user")
+    }
+    if model.password, ok = config["password"]; !ok {
+        return missingVolumeContextKeyError("password")
+    }
+    return nil
 }
 
 func (ns *nodeServer) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpublishVolumeRequest) (*csi.NodeUnpublishVolumeResponse, error) {
-	targetPath := req.GetTargetPath()
-	notMnt, err := mount.New("").IsLikelyNotMountPoint(targetPath)
+    targetPath := req.GetTargetPath()
+    notMnt, err := mount.New("").IsLikelyNotMountPoint(targetPath)
 
-	if err != nil {
-		if os.IsNotExist(err) {
-			return nil, status.Error(codes.NotFound, "Targetpath not found")
-		} else {
-			return nil, status.Error(codes.Internal, err.Error())
-		}
-	}
-	if notMnt {
-		return nil, status.Error(codes.NotFound, "Volume not mounted")
-	}
+    if err != nil {
+        if os.IsNotExist(err) {
+            return nil, status.Error(codes.NotFound, "Targetpath not found")
+        } else {
+            return nil, status.Error(codes.Internal, err.Error())
+        }
+    }
+    if notMnt {
+        return nil, status.Error(codes.NotFound, "Volume not mounted")
+    }
 
-	err = util.UnmountPath(req.GetTargetPath(), mount.New(""))
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-	if point, ok := ns.mounts[req.VolumeId]; ok {
-		err := os.Remove(point.IdentityFile)
-		if err != nil {
-			return nil, status.Error(codes.Internal, err.Error())
-		}
-		delete(ns.mounts, point.VolumeId)
-		glog.Infof("successfully unmount volume: %s", point)
-	}
+    err = util.UnmountPath(req.GetTargetPath(), mount.New(""))
+    if err != nil {
+        return nil, status.Error(codes.Internal, err.Error())
+    }
+    if point, ok := ns.mounts[req.VolumeId]; ok {
+        if point.IdentityFile != "" {
+            err := os.Remove(point.IdentityFile)
+            if err != nil {
+                return nil, status.Error(codes.Internal, err.Error())
+            }
+        }
 
-	return &csi.NodeUnpublishVolumeResponse{}, nil
+        delete(ns.mounts, point.VolumeId)
+        glog.Infof("successfully unmount volume: %s", point)
+    }
+
+    return &csi.NodeUnpublishVolumeResponse{}, nil
 }
 
 func (ns *nodeServer) NodeUnstageVolume(ctx context.Context, req *csi.NodeUnstageVolumeRequest) (*csi.NodeUnstageVolumeResponse, error) {
-	return &csi.NodeUnstageVolumeResponse{}, nil
+    return &csi.NodeUnstageVolumeResponse{}, nil
 }
 
 func (ns *nodeServer) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolumeRequest) (*csi.NodeStageVolumeResponse, error) {
-	return &csi.NodeStageVolumeResponse{}, nil
-}
-
-func validateVolumeContext(req *csi.NodePublishVolumeRequest) error {
-	if _, ok := req.GetVolumeContext()["server"]; !ok {
-		return status.Errorf(codes.InvalidArgument, "missing volume context value: server")
-	}
-	if _, ok := req.GetVolumeContext()["user"]; !ok {
-		return status.Errorf(codes.InvalidArgument, "missing volume context value: user")
-	}
-	if _, ok := req.GetVolumeContext()["share"]; !ok {
-		return status.Errorf(codes.InvalidArgument, "missing volume context value: share")
-	}
-
-	_, pkOk := req.GetVolumeContext()["privateKey"]
-	_, pwOk := req.GetVolumeContext()["password"]
-	if !(pkOk || pwOk) {
-		return status.Errorf(codes.InvalidArgument, "missing volume context value: privateKey or password")
-	}
-	return nil
+    return &csi.NodeStageVolumeResponse{}, nil
 }
 
 func getPublicKeySecret(secretName string) (*v1.Secret, error) {
-	namespaceAndSecret := strings.SplitN(secretName, "/", 2)
-	namespace := namespaceAndSecret[0]
-	name := namespaceAndSecret[1]
+    secret, err := getSecret(secretName)
+    if err != nil {
+        return secret, err
+    }
 
-	clientset, e := GetK8sClient()
-	if e != nil {
-		return nil, status.Errorf(codes.Internal, "can not create kubernetes client: %s", e)
-	}
+    if secret.Type != v1.SecretTypeSSHAuth {
+        return nil, status.Errorf(codes.InvalidArgument, "type of secret %s is not %s", secretName, v1.SecretTypeSSHAuth)
+    }
 
-	secret, e := clientset.CoreV1().
-		Secrets(namespace).
-		Get(name, metav1.GetOptions{})
-
-	if e != nil {
-		return nil, status.Errorf(codes.Internal, "can not get secret %s: %s", secretName, e)
-	}
-
-	if secret.Type != v1.SecretTypeSSHAuth {
-		return nil, status.Errorf(codes.InvalidArgument, "type of secret %s is not %s", secretName, v1.SecretTypeSSHAuth)
-	}
-	return secret, nil
+    return secret, nil
 }
 
-func writePrivateKey(secret *v1.Secret) (string, error) {
-	f, e := ioutil.TempFile("", "pk-*")
-	defer f.Close()
-	if e != nil {
-		return "", status.Errorf(codes.Internal, "can not create tmp file for pk: %s", e)
-	}
+func getSecret(secretName string) (*v1.Secret, error) {
+    namespaceAndSecret := strings.SplitN(secretName, "/", 2)
+    namespace := namespaceAndSecret[0]
+    name := namespaceAndSecret[1]
 
-	_, e = f.Write(secret.Data[v1.SSHAuthPrivateKey])
-	if e != nil {
-		return "", status.Errorf(codes.Internal, "can not create tmp file for pk: %s", e)
-	}
-	e = f.Chmod(0600)
-	if e != nil {
-		return "", status.Errorf(codes.Internal, "can not change rights for pk: %s", e)
-	}
-	return f.Name(), nil
+    clientset, e := GetK8sClient()
+    if e != nil {
+        return nil, status.Errorf(codes.Internal, "can not create kubernetes client: %s", e)
+    }
+
+    secret, e := clientset.CoreV1().
+        Secrets(namespace).
+        Get(name, metav1.GetOptions{})
+
+    if e != nil {
+        return nil, status.Errorf(codes.Internal, "can not get secret %s: %s", secretName, e)
+    }
+    return secret, nil
 }
 
-func parseRawOpts(result map[string]string, sshOpts string) {
-	if sshOpts == "" {
-		return
-	}
+func writePrivateKey(privateKey []byte) (string, error) {
+    f, e := ioutil.TempFile("", "pk-*")
+    defer f.Close()
+    if e != nil {
+        return "", status.Errorf(codes.Internal, "can not create tmp file for pk: %s", e)
+    }
 
-	splitOpts := strings.Split(sshOpts, ";")
-	for _, item := range splitOpts {
-		parts := strings.SplitN(item, "=", 2)
-		switch len(parts) {
-		case 0:
-			continue
-		case 1:
-			result[parts[0]] = ""
-		case 2:
-			result[parts[0]] = parts[1]
-		}
-	}
+    _, e = f.Write(privateKey)
+    if e != nil {
+        return "", status.Errorf(codes.Internal, "can not create tmp file for pk: %s", e)
+    }
+    e = f.Chmod(0600)
+    if e != nil {
+        return "", status.Errorf(codes.Internal, "can not change rights for pk: %s", e)
+    }
+    return f.Name(), nil
 }
 
-func Mount(user, host, port, dir, target, password, privateKey, sshOpts string) error {
-	mountCmd := "sshfs"
-	mountArgs := generateMountArgs(user, host, port, dir, target, password, privateKey, sshOpts)
+func parseRawOpts(sshOpts string) map[string]string {
+    result := make(map[string]string)
+    if sshOpts == "" {
+        return result
+    }
 
-	// create target, os.Mkdirall is noop if it exists
-	err := os.MkdirAll(target, 0750)
-	if err != nil {
-		return err
-	}
+    splitOpts := strings.Split(sshOpts, ";")
+    for _, item := range splitOpts {
+        parts := strings.SplitN(item, "=", 2)
+        switch len(parts) {
+        case 0:
+            continue
+        case 1:
+            result[parts[0]] = ""
+        case 2:
+            result[parts[0]] = parts[1]
+        }
+    }
 
-	glog.Infof("executing mount command cmd=%s, args=%s", mountCmd, mountArgs)
-
-	cmd := exec.Command(mountCmd, mountArgs...)
-
-	if password != "" {
-		if err = pipePasswordIn(cmd, password); err != nil {
-			glog.Errorf("failed to get std in from sshfs: %v", err)
-			return err
-		}
-	}
-
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("mounting failed: %v cmd: '%s %s' output: %q",
-			err, mountCmd, strings.Join(mountArgs, " "), string(out))
-	}
-
-	return nil
+    return result
 }
 
-func generateMountArgs(user, host, port, dir, target, password, privateKey, sshOpts string) []string {
+func Mount(config config, target string) error {
+    mountCmd := "sshfs"
+    mountArgs := generateMountArgs(config, target)
 
-	optsMap := map[string]string{
-		"StrictHostKeyChecking": "accept-new",
-		"UserKnownHostsFile":    "/dev/null",
-		"allow_other":           "",
-		"uid":                   "100",
-		"gid":                   "0",
-		"reconnect":             "",
-		"ServerAliveInterval":   "15",
-		"ServerAliveCountMax":   "3",
-		"port":                  port,
-	}
+    // create target, os.Mkdirall is noop if it exists
+    err := os.MkdirAll(target, 0750)
+    if err != nil {
+        return err
+    }
 
-	if privateKey != "" {
-		optsMap["IdentityFile"] = privateKey
-	}
+    glog.Infof("executing mount command cmd=%s, args=%s", mountCmd, mountArgs)
 
-	if password != "" {
-		optsMap["password_stdin"] = ""
-	}
+    cmd := exec.Command(mountCmd, mountArgs...)
 
-	parseRawOpts(optsMap, sshOpts)
+    if config.password != "" {
+        if err = pipePasswordIn(cmd, config.password); err != nil {
+            glog.Errorf("failed to get std in from sshfs: %v", err)
+            return err
+        }
+    }
 
-	source := fmt.Sprintf("%s@%s:%s", user, host, dir)
-	mountArgs := []string{source, target}
+    out, err := cmd.CombinedOutput()
+    if err != nil {
+        return fmt.Errorf("mounting failed: %v cmd: '%s %s' output: %q",
+            err, mountCmd, strings.Join(mountArgs, " "), string(out))
+    }
 
-	// just to simplify testing
-	sortedKeys := getSortedKeys(optsMap)
-	for _, key := range sortedKeys {
-		val := optsMap[key]
-		var opt string
-		if val != "" {
-			opt = fmt.Sprintf("%s=%s", key, val)
-		} else {
-			opt = key
-		}
-		mountArgs = append(mountArgs, "-o", opt)
-	}
-	return mountArgs
+    return nil
+}
+
+func generateMountArgs(config config, target string) []string {
+
+    optsMap := map[string]string{
+        "StrictHostKeyChecking": "accept-new",
+        "UserKnownHostsFile":    "/dev/null",
+        "allow_other":           "",
+        "uid":                   "100",
+        "gid":                   "0",
+        "reconnect":             "",
+        "ServerAliveInterval":   "15",
+        "ServerAliveCountMax":   "3",
+        "port":                  config.port,
+    }
+
+    if config.privateKeyPath != "" {
+        optsMap["IdentityFile"] = config.privateKeyPath
+    }
+
+    if config.password != "" {
+        optsMap["password_stdin"] = ""
+    }
+
+    for key, val := range config.sshOptsMap {
+        optsMap[key] = val
+    }
+
+    source := fmt.Sprintf("%s@%s:%s", config.username, config.server, config.share)
+    mountArgs := []string{source, target}
+
+    // just to simplify testing
+    sortedKeys := getSortedKeys(optsMap)
+    for _, key := range sortedKeys {
+        val := optsMap[key]
+        var opt string
+        if val != "" {
+            opt = fmt.Sprintf("%s=%s", key, val)
+        } else {
+            opt = key
+        }
+        mountArgs = append(mountArgs, "-o", opt)
+    }
+    return mountArgs
 }
 
 func getSortedKeys(optsMap map[string]string) []string {
-	var keys []string
-	for key := range optsMap {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys)
-	return keys
+    var keys []string
+    for key := range optsMap {
+        keys = append(keys, key)
+    }
+    sort.Strings(keys)
+    return keys
 }
 
 func pipePasswordIn(cmd *exec.Cmd, password string) error {
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		return err
-	}
+    stdin, err := cmd.StdinPipe()
+    if err != nil {
+        return err
+    }
 
-	defer stdin.Close()
+    defer stdin.Close()
 
-	count, err := io.WriteString(stdin, password)
-	if err != nil {
-		return err
-	}
+    count, err := io.WriteString(stdin, password)
+    if err != nil {
+        return err
+    }
 
-	glog.Infof("wrote %d password bytes", count)
+    glog.Infof("wrote %d password bytes", count)
 
-	return nil
+    return nil
 }
--- pkg/sshfs/nodeserver_test.go
+++ pkg/sshfs/nodeserver_test.go
@@ -1,73 +1,75 @@
 package sshfs
 
 import (
-	"testing"
+    "testing"
 
-	"github.com/stretchr/testify/require"
+    "github.com/stretchr/testify/require"
 )
 
 func TestParseRawOpts(t *testing.T) {
-	t.Run("can omit private key", func(t *testing.T) {
-		results := make(map[string]string)
-		parseRawOpts(results, "")
-		require.Equal(t, map[string]string{}, results)
-	})
+    t.Run("can omit private key", func(t *testing.T) {
+        results := parseRawOpts("")
+        require.Equal(t, map[string]string{}, results)
+    })
 }
 
 func TestGenerateMountArgs(t *testing.T) {
-	t.Run("private key example", func(t *testing.T) {
-		result := generateMountArgs("user", "host", "port", "directory", "target", "", "private-key", "ssh-opt1;ssh-opt2=hello world")
-		require.Equal(t, []string{
-			"user@host:directory", "target",
-			"-o", "IdentityFile=private-key",
-			"-o", "ServerAliveCountMax=3",
-			"-o", "ServerAliveInterval=15",
-			"-o", "StrictHostKeyChecking=accept-new",
-			"-o", "UserKnownHostsFile=/dev/null",
-			"-o", "allow_other",
-			"-o", "gid=0",
-			"-o", "port=port",
-			"-o", "reconnect",
-			"-o", "ssh-opt1",
-			"-o", "ssh-opt2=hello world",
-			"-o", "uid=100",
-		}, result)
-	})
+    t.Run("private key example", func(t *testing.T) {
+        config := config{"host", "port", "directory", map[string]string{"ssh-opt1": "", "ssh-opt2": "hello world"}, "user", "", "private-key"}
+        result := generateMountArgs(config, "target")
+        require.Equal(t, []string{
+            "user@host:directory", "target",
+            "-o", "IdentityFile=private-key",
+            "-o", "ServerAliveCountMax=3",
+            "-o", "ServerAliveInterval=15",
+            "-o", "StrictHostKeyChecking=accept-new",
+            "-o", "UserKnownHostsFile=/dev/null",
+            "-o", "allow_other",
+            "-o", "gid=0",
+            "-o", "port=port",
+            "-o", "reconnect",
+            "-o", "ssh-opt1",
+            "-o", "ssh-opt2=hello world",
+            "-o", "uid=100",
+        }, result)
+    })
 
-	t.Run("password example", func(t *testing.T) {
-		result := generateMountArgs("user", "host", "port", "directory", "target", "a-password", "", "ssh-opt1;ssh-opt2=hello world")
-		require.Equal(t, []string{
-			"user@host:directory", "target",
-			"-o", "ServerAliveCountMax=3",
-			"-o", "ServerAliveInterval=15",
-			"-o", "StrictHostKeyChecking=accept-new",
-			"-o", "UserKnownHostsFile=/dev/null",
-			"-o", "allow_other",
-			"-o", "gid=0",
-			"-o", "password_stdin",
-			"-o", "port=port",
-			"-o", "reconnect",
-			"-o", "ssh-opt1",
-			"-o", "ssh-opt2=hello world",
-			"-o", "uid=100",
-		}, result)
-	})
+    t.Run("password example", func(t *testing.T) {
+        config := config{"host", "port", "directory", map[string]string{"ssh-opt1": "", "ssh-opt2": "hello world"}, "user", "p4ssw0rd", ""}
+        result := generateMountArgs(config, "target")
+        require.Equal(t, []string{
+            "user@host:directory", "target",
+            "-o", "ServerAliveCountMax=3",
+            "-o", "ServerAliveInterval=15",
+            "-o", "StrictHostKeyChecking=accept-new",
+            "-o", "UserKnownHostsFile=/dev/null",
+            "-o", "allow_other",
+            "-o", "gid=0",
+            "-o", "password_stdin",
+            "-o", "port=port",
+            "-o", "reconnect",
+            "-o", "ssh-opt1",
+            "-o", "ssh-opt2=hello world",
+            "-o", "uid=100",
+        }, result)
+    })
 
-	t.Run("can override defaults", func(t *testing.T) {
-		result := generateMountArgs("user", "host", "port", "directory", "target", "a-password", "", "uid=33;gid=33")
-		require.Equal(t, []string{
-			"user@host:directory", "target",
-			"-o", "ServerAliveCountMax=3",
-			"-o", "ServerAliveInterval=15",
-			"-o", "StrictHostKeyChecking=accept-new",
-			"-o", "UserKnownHostsFile=/dev/null",
-			"-o", "allow_other",
-			"-o", "gid=33",
-			"-o", "password_stdin",
-			"-o", "port=port",
-			"-o", "reconnect",
-			"-o", "uid=33",
-		}, result)
-	})
+    t.Run("can override defaults", func(t *testing.T) {
+        config := config{"host", "port", "directory", map[string]string{"gid": "33", "uid": "33"}, "user", "password", ""}
+        result := generateMountArgs(config, "target")
+        require.Equal(t, []string{
+            "user@host:directory", "target",
+            "-o", "ServerAliveCountMax=3",
+            "-o", "ServerAliveInterval=15",
+            "-o", "StrictHostKeyChecking=accept-new",
+            "-o", "UserKnownHostsFile=/dev/null",
+            "-o", "allow_other",
+            "-o", "gid=33",
+            "-o", "password_stdin",
+            "-o", "port=port",
+            "-o", "reconnect",
+            "-o", "uid=33",
+        }, result)
+    })
 
 }
